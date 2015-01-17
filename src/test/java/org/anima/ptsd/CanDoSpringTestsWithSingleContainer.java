b4f97f0bee5a94d3d8f855e971761cc71722434e
Tsukihara Caligin <caligin35@gmail.com>
2015-01-17 19:06:43 +0100

    enh: can run a test in temporary container

--- src/test/java/org/anima/ptsd/CanDoSpringTestsWithSingleContainer.java
+++ src/test/java/org/anima/ptsd/CanDoSpringTestsWithSingleContainer.java
@@ -1,6 +1,22 @@
 package org.anima.ptsd;
 
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerCertificateException;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.DockerException;
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.ContainerCreation;
+import com.spotify.docker.client.messages.ContainerInfo;
+import com.spotify.docker.client.messages.HostConfig;
+import com.spotify.docker.client.messages.Image;
+import com.spotify.docker.client.messages.PortBinding;
+import java.io.Closeable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.logging.Logger;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.sql.DataSource;
@@ -47,14 +63,67 @@ public class CanDoSpringTestsWithSingleContainer {
         Assert.assertEquals("asd", got.getValue());
     }
 
+    public static class PostgresContainer implements Closeable {
+
+        public final String id;
+        public final String host;
+        public final String port;
+        private final DockerClient client;
+
+        public PostgresContainer(String id, String host, String port, DockerClient client) {
+            this.id = id;
+            this.host = host;
+            this.port = port;
+            this.client = client;
+        }
+
+        @Override
+        public void close() {
+            try {
+                client.stopContainer(id, 5);
+                client.removeContainer(id);
+            } catch (DockerException | InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
     @Configuration
     public static class Config {
 
-        // docker run --name some-postgres --rm -p 127.0.0.1:15432:5432 -e POSTGRES_PASSWORD="" postgres:9.4
+        @Bean
+        public PostgresContainer postgresContainer() throws DockerCertificateException, DockerException, InterruptedException {
+            final String host = "127.0.0.1";
+            final String port = "15432";
+            final DockerClient docker = DefaultDockerClient.fromEnv().build();
+            final List<Image> images = docker.listImages();
+            boolean alreadyPulled = images.stream().anyMatch(image -> image.repoTags().contains("postgres:9.4"));
+            if (!alreadyPulled) {
+                Logger.getAnonymousLogger().info("pulling");
+                docker.pull("postgres:9.4");
+                Logger.getAnonymousLogger().info("done pulling");
+            }
+            final ContainerConfig config = ContainerConfig.builder()
+                    .image("postgres:9.4")
+                    .env("POSTGRES_PASSWORD=")
+                    .portSpecs(String.format("%s:%s:5432", host, port))
+                    .build();
+            final ContainerCreation creation = docker.createContainer(config);
+            final String id = creation.id();
+
+            final Map<String, List<PortBinding>> portBindings = new HashMap<>();
+            portBindings.put("5432/tcp", Arrays.asList(PortBinding.of(host, port)));
+            final HostConfig hostConfig = HostConfig.builder()
+                    .portBindings(portBindings)
+                    .build();
+            docker.startContainer(id, hostConfig);
+            Thread.sleep(15000); // temporary hack. container initializes postgres before accepting connections on pgport, must wait until ready or fail because can't connect
+            return new PostgresContainer(id, host, port, docker);
+        }
 
         @Bean
-        public DataSource dataSource() {
-            return new SimpleDriverDataSource(new Driver(), "jdbc:postgresql://localhost:15432/", "postgres", ""); // TODO: use a test db, not the default one and ensure that it is created
+        public DataSource dataSource(PostgresContainer container) {
+            return new SimpleDriverDataSource(new Driver(), String.format("jdbc:postgresql://%s:%s/", container.host, container.port), "postgres", ""); // TODO: use a test db, not the default one and ensure that it is created
         }
 
         @Bean
